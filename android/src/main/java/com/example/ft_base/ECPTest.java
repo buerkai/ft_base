e3b7c2e2f4d95eff787e9d40664b296503d960fd
goodluck <851392159@qq.com>
2019-10-12 10:34:49 +0800

    测试代码

---
+++ android/src/main/java/com/example/ft_base/ECPTest.java
@@ -0,0 +1,11 @@
+package com.example.ft_base;
+
+public class ECPTest {
+
+    public static String test(String test) {
+        System.out.println("=====================");
+        System.out.println("test=" + test);
+        System.out.println("=====================");
+        return "a=" + test;
+    }
+}
